fe0b2f6bcb11a3e80b3f605e218b0d76578ab74f
vickyfto <vickyfitrio19@gmail.com>
2020-10-27 04:17:03 +0700

    .

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 function darkMode() {
     document.body.classList.toggle('bg-dark');
     document.body.style.transition = '0.8s';
-}
 
+    if( document.body.classList.contains('bg-dark') == true) {
+        document.querySelector('.btn-bg').innerHTML = 'white mode';
+        const colorText = document.querySelectorAll('div,h2,a');
+        for(let i = 0; i < colorText.length; i++)
+            colorText[i].style.color = 'white';
+        // console.log(colorText)
+    }
+    else {
+        // let bool = Boolean
+        document.querySelector('.btn-bg').innerHTML = 'dark mode';
+        const colorText = document.querySelectorAll('div,h2,a');
+        for(let i = 0; i < colorText.length; i++)
+            colorText[i].style.color = 'black';
+        // console.log(colorText)
+    }
+}
